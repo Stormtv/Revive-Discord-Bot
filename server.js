c82df99c0f49052963dd6cb47d5ba2a5043fbada
Tyler Storm <stormtv@mail.uc.edu>
2017-03-13 22:58:44 -0400

    Trying to fix bugs

--- server.js
+++ server.js
@@ -8,8 +8,6 @@ const request = require('request');
 const bodyParser = require('body-parser');
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const async = require('asyncawait/async');
-const await = require('asyncawait/await');
 app.set('serverPort', myConfig.serverPort);
 app.use(bodyParser.urlencoded({
   extended: true
@@ -91,66 +89,65 @@ app.post('/createGuildApp', (req,res) => {
     joke:req.body.joke
   };
   const applicationText = `**What is your Name, Age and Sex?**\n
-    ━━━━━━━━━━━━━━━━━━━━━━━━\n
-    ${applicationData.name}\n\n
-    **Provide the location you will primarily will be playing from and a speedtest.net screenshot from the Chicago server**\n
-    ━━━━━━━━━━━━━━━━━━━━━━━━\n
-    ${applicationData.loc}\n\n
-    **What is your Battle.net Tag**\n
-    ━━━━━━━━━━━━━━━━━━━━━━━━\n
-    ${applicationData.tag}\n\n
-    **Link to your main characters and alternate character's ADVANCED ARMORY**\n
-    ━━━━━━━━━━━━━━━━━━━━━━━━\n
-    ${applicationData.armory}\n\n
-    **Post a screenshot of your character in combat**\n
-    ━━━━━━━━━━━━━━━━━━━━━━━━\n
-    ${applicationData.ss}\n\n
-    **Do you have a functioning headset with microphone that you can use in Discord?**\n
-    ━━━━━━━━━━━━━━━━━━━━━━━━\n
-    ${applicationData.headset}\n\n
-    **Post your recent warcraft logs**\n
-    ━━━━━━━━━━━━━━━━━━━━━━━━\n
-    ${applicationData.logs}\n\n
-    **What role and spec are you applying for**\n
-    ━━━━━━━━━━━━━━━━━━━━━━━━\n
-    ${applicationData.role}\n\n
-    **List your Artifact Weapon points for all Specs **\n
-    ━━━━━━━━━━━━━━━━━━━━━━━━\n
-    ${applicationData.awp}\n\n
-    **List Pre Tier-19 Mythic/Heroic Progression Experiences**\n
-    ━━━━━━━━━━━━━━━━━━━━━━━━\n
-    ${applicationData.prog}\n\n
-    **Name your last TWO raiding guilds and the reason for leaving them?**\n
-    ━━━━━━━━━━━━━━━━━━━━━━━━\n
-    ${applicationData.left}\n\n
-    **Take us through your opening rotation on a SINGLE TARGET fight. (Healers Ignore this)**\n
-    ━━━━━━━━━━━━━━━━━━━━━━━━\n
-    ${applicationData.rotation}\n\n
-    **What resources do you use to improve yourself?**\n
-    ━━━━━━━━━━━━━━━━━━━━━━━━\n
-    ${applicationData.resources}\n\n
-    **How did you hear about us?**\n
-    ━━━━━━━━━━━━━━━━━━━━━━━━\n
-    ${applicationData.hear}\n\n
-    **How do you handle criticism and adult humor?**\n
-    ━━━━━━━━━━━━━━━━━━━━━━━━\n
-    ${applicationData.crit}\n\n
-    **You may be asked to sit on some fights for composition, gear, or general skill level how do you feel about that?**\n
-    ━━━━━━━━━━━━━━━━━━━━━━━━\n
-    ${applicationData.sit}\n\n
-    **Do you have any commitments or schedule changes that we should know about?**\n
-    ━━━━━━━━━━━━━━━━━━━━━━━━\n
-    ${applicationData.schedule}\n\n
-    **Are you willing to test new content in PTR/Alpha/Beta?**\n
-    ━━━━━━━━━━━━━━━━━━━━━━━━\n
-    ${applicationData.beta}\n\n
-    **Tell us a little bit about yourself. The more we know the better!**\n
-    ━━━━━━━━━━━━━━━━━━━━━━━━\n
-    ${applicationData.about}\n\n
-    **Tell us a joke**\n
-    ━━━━━━━━━━━━━━━━━━━━━━━━\n
-    ${applicationData.joke}\n\n
-    `;
+━━━━━━━━━━━━━━━━━━━━━━━━\n
+${applicationData.name}\n\n
+**Provide the location you will primarily will be playing from and a speedtest.net screenshot from the Chicago server**\n
+━━━━━━━━━━━━━━━━━━━━━━━━\n
+${applicationData.loc}\n\n
+**What is your Battle.net Tag**\n
+━━━━━━━━━━━━━━━━━━━━━━━━\n
+${applicationData.tag}\n\n
+**Link to your main characters and alternate character's ADVANCED ARMORY**\n
+━━━━━━━━━━━━━━━━━━━━━━━━\n
+${applicationData.armory}\n\n
+**Post a screenshot of your character in combat**\n
+━━━━━━━━━━━━━━━━━━━━━━━━\n
+${applicationData.sscombat}\n\n
+**Do you have a functioning headset with microphone that you can use in Discord?**\n
+━━━━━━━━━━━━━━━━━━━━━━━━\n
+${applicationData.headset}\n\n
+**Post your recent warcraft logs**\n
+━━━━━━━━━━━━━━━━━━━━━━━━\n
+${applicationData.logs}\n\n
+**What role and spec are you applying for**\n
+━━━━━━━━━━━━━━━━━━━━━━━━\n
+${applicationData.role}\n\n
+**List your Artifact Weapon points for all Specs **\n
+━━━━━━━━━━━━━━━━━━━━━━━━\n
+${applicationData.awp}\n\n
+**List Pre Tier-19 Mythic/Heroic Progression Experiences**\n
+━━━━━━━━━━━━━━━━━━━━━━━━\n
+${applicationData.prog}\n\n
+**Name your last TWO raiding guilds and the reason for leaving them?**\n
+━━━━━━━━━━━━━━━━━━━━━━━━\n
+${applicationData.left}\n\n
+**Take us through your opening rotation on a SINGLE TARGET fight. (Healers Ignore this)**\n
+━━━━━━━━━━━━━━━━━━━━━━━━\n
+${applicationData.rotation}\n\n
+**What resources do you use to improve yourself?**\n
+━━━━━━━━━━━━━━━━━━━━━━━━\n
+${applicationData.resources}\n\n
+**How did you hear about us?**\n
+━━━━━━━━━━━━━━━━━━━━━━━━\n
+${applicationData.hear}\n\n
+**How do you handle criticism and adult humor?**\n
+━━━━━━━━━━━━━━━━━━━━━━━━\n
+${applicationData.crit}\n\n
+**You may be asked to sit on some fights for composition, gear, or general skill level how do you feel about that?**\n
+━━━━━━━━━━━━━━━━━━━━━━━━\n
+${applicationData.sit}\n\n
+**Do you have any commitments or schedule changes that we should know about?**\n
+━━━━━━━━━━━━━━━━━━━━━━━━\n
+${applicationData.schedule}\n\n
+**Are you willing to test new content in PTR/Alpha/Beta?**\n
+━━━━━━━━━━━━━━━━━━━━━━━━\n
+${applicationData.beta}\n\n
+**Tell us a little bit about yourself. The more we know the better!**\n
+━━━━━━━━━━━━━━━━━━━━━━━━\n
+${applicationData.about}\n\n
+**Tell us a joke**\n
+━━━━━━━━━━━━━━━━━━━━━━━━\n
+${applicationData.joke}\n\n`;
   let guild = bot.guilds.find(val => val.id === myConfig.guildID);
   let _this = this;
   if (guild.available) {
@@ -166,11 +163,15 @@ app.post('/createGuildApp', (req,res) => {
          temporary: false,
          maxUses: 1
        }).then(invite => {
+         console.log("About to send message");
          channel.sendMessage(applicationText, {split:true})
-          .then(message => console.log("sent message"))
+          .then(message => {
+            console.log("sent message");
+          })
           .catch(console.error);
           console.log(`Sending invite url: ${invite.url} `);
-         res.send(invite.url);
+          botInvites.push(invite.code);
+          res.send(invite.url);
        })
        .catch(console.error);
      })
